24a005522c49c9caee903a72c4e5bcc5477d24a8
Alex Glaubitz <e.pirazzi@gmail.com>
2023-04-07 13:45:14 -0400

    modal opens and closes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const showModal = ref(false)
+</script>
+
 <template>
   <main>
-    <div class="overlay">
+    <div v-if="showModal" class="overlay">
       <div class="modal">
         <textarea name="note" id="note" cols="30" rows="10"></textarea>
         <button>Add Note</button>
-        <button class="close">Close</button>
+        <button class="close" @click="showModal = false">Close</button>
       </div>
     </div>
     <div class="container">
